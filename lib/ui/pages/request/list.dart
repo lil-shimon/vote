dc193903f3d51381c12839baa5a116eb037957ea
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-01-25 23:55:54 +0900

    feat: 🎸 clean up main.dart

---
+++ lib/ui/pages/request/list.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class RequestListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Request List'),
+      ),
+      body: Center(
+        child: Text('Request List'),
+      ),
+    );
+  }
+}
